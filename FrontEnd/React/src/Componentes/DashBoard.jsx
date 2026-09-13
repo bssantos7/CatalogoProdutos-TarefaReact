4980e16eee91b83cecf6c2dbc3f3a80549ad3c9b
bs.santos7 <bs.santos7@hotmail.com>
2026-09-13 10:07:48 -0300

    Finalizado. pequena correção

--- FrontEnd/React/src/Componentes/DashBoard.jsx
+++ FrontEnd/React/src/Componentes/DashBoard.jsx
@@ -3,7 +3,7 @@ import Card from "./Card";
 function DashBoard({listaProdutos}){
     const lista=[...listaProdutos];
     return(
-        <div className="container">
+        <div>
             {lista.map((produto)=>(
                 <Card key={produto.codigo} idCard={produto.codigo} src={produto.imagem} titulo={produto.nome} preco={produto.preco} descricao={produto.descricao}/>
             ))}
